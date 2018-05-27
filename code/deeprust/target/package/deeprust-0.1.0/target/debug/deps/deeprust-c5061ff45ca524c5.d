eaa89bf69a70166edb92fd77ba2291fb80a3eef6
dvigneshwer <dvigneshwer@gmail.com>
2018-05-27 21:29:30 +0530

    changes to manifest file

--- code/deeprust/target/package/deeprust-0.1.0/target/debug/deps/deeprust-c5061ff45ca524c5.d
+++
@@ -1,9 +0,0 @@
-/home/vigneshwer/github_project_repos/deeprust/code/deeprust/target/package/deeprust-0.1.0/target/debug/deps/libdeeprust-c5061ff45ca524c5.rlib: src/lib.rs src/metrics/mod.rs src/metrics/confusion_matrix.rs src/reg/mod.rs src/reg/regularizer.rs
-
-/home/vigneshwer/github_project_repos/deeprust/code/deeprust/target/package/deeprust-0.1.0/target/debug/deps/deeprust-c5061ff45ca524c5.d: src/lib.rs src/metrics/mod.rs src/metrics/confusion_matrix.rs src/reg/mod.rs src/reg/regularizer.rs
-
-src/lib.rs:
-src/metrics/mod.rs:
-src/metrics/confusion_matrix.rs:
-src/reg/mod.rs:
-src/reg/regularizer.rs:
