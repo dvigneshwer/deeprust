a18555d18877f3709e9eb9a0bfb3aa52c82bfaf8
dvigneshwer <dvigneshwer@gmail.com>
2018-05-27 21:25:57 +0530

    changes to manifest file

---
+++ code/deeprust/target/package/deeprust-0.1.0/src/reg/mod.rs
@@ -0,0 +1,10 @@
+// Task : Main file for executing the different regularizer units
+// Author : Vigneshwer 
+// Date : 18th Feb 16
+// Version : 0.0.1
+
+
+//! Module for performing regularization operations 
+
+/// Functions inside these modules are used to penalize the update inorder to avoid over fitting and making a generalized module
+pub mod regularizer;
